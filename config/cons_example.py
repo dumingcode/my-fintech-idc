82cf6d7be72d1d03162ab4d87e655513135a015d
dumingcode <jake1036@126.com>
2019-06-15 18:37:39 +0800

    修改uri

--- config/cons_example.py
+++ config/cons_example.py
@@ -2,7 +2,7 @@ CONFIG = {
     'ENV': 'dev',
     'TOKEN': 'xx',
     'MONGODB': {
-        'uri': 'mongodb://127.0.0.1:27017/?authSource=stock'
+        'uri': 'mongodb://127.0.0.1:27017/stock'
     },
     # 更新BACK_TRACK_DAYS天前的股票前复权数据
     'BACK_TRACK_DAYS': 600}
